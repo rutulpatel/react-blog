0cd9c25015271bd6952547f7e5204677eb1d2bb3
Rutul Patel <rutulpatel@Rutuls-MacBook-Pro.local>
2018-03-08 09:03:20 -0600

    Add delete a post func.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
@@ -9,6 +9,13 @@ class PostsShow extends Component {
             const { id } = this.props.match.params;
             this.props.fetchPost(id);
         }
+    }
+
+    onDeleteClick() {
+        const { id } = this.props.match.params;        
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -21,6 +28,11 @@ class PostsShow extends Component {
         return (
             <div>
                 <Link to="/"> Back to Index</Link>
+                <button 
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)} >
+                        Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -35,4 +47,4 @@ function mapStateToProps({ posts }, ownProps) {
     }
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
